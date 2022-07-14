72374bb300fc27ac1dfd5bd416c17a765009eafc
john <ajbaptist18@gmail.com>
2022-07-14 18:38:35 +0530

    impt

--- lib/controller/demo_controller.dart
+++ lib/controller/demo_controller.dart
@@ -1,22 +1,37 @@
+import 'dart:convert';
+
 import 'package:demo/model/demo_model.dart';
 import 'package:demo/service/demo_service.dart';
 import 'package:get/state_manager.dart';
+import 'package:get_storage/get_storage.dart';
 
 class DemoController extends GetxController {
+  static final box = GetStorage();
 
-  var list=<Datum>[].obs;
+  @override
+  void onInit() {
+    fetchbox();
+    super.onInit();
+  }
 
-  fetchService()async{
+  var list = <Datum>[].obs;
 
-    final result =await DemoService.demoService();
-    if (result!=null) {
-      list.value=result.data;
-      
+  fetchService() async {
+    final result = await DemoService.demoService();
+    if (result != null) {
+      list.value = result.data;
+      box.write("i", result.data);
     } else {
       throw Exception(result);
-      
     }
-
   }
-  
+
+  fetchbox() async {
+    final result = box.read("i");
+
+    if (result != null) {
+      List data =  result;
+      list.value = data.map<Datum>((o) => Datum.fromJson(o)).toList();
+    } else {}
+  }
 }
